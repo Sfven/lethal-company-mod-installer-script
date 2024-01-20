2566b1d4b4d9a5cc0105b4a083d31376eaef5702
Sfven <dat.dat82@gmail.com>
2024-01-19 20:16:42 -0600

    add some, and update all mods

--- install-script.bat
+++ install-script.bat
@@ -7,18 +7,19 @@ echo ------------------------------------------------------
 set "defaultDir=C:\Program Files (x86)\Steam\steamapps\common\Lethal Company"
 set "downloadFolder=%USERPROFILE%\Downloads\"
 
-set "urls[0]=https://gcdn.thunderstore.io/live/repository/packages/notnotnotswipez-MoreCompany-1.7.4.zip"
-set "urls[1]=https://gcdn.thunderstore.io/live/repository/packages/2018-LC_API-3.3.2.zip"
-set "urls[2]=https://gcdn.thunderstore.io/live/repository/packages/Sligili-More_Emotes-1.3.2.zip"
+set "urls[0]=https://gcdn.thunderstore.io/live/repository/packages/notnotnotswipez-MoreCompany-1.7.5.zip"
+set "urls[1]=https://gcdn.thunderstore.io/live/repository/packages/2018-LC_API-3.4.5.zip"
+set "urls[2]=https://gcdn.thunderstore.io/live/repository/packages/Sligili-More_Emotes-1.3.3.zip"
 set "urls[3]=https://gcdn.thunderstore.io/live/repository/packages/Ozone-Runtime_Netcode_Patcher-0.2.5.zip"
 set "urls[4]=https://gcdn.thunderstore.io/live/repository/packages/sunnobunno-YippeeMod-1.2.3.zip"
-set "urls[5]=https://gcdn.thunderstore.io/live/repository/packages/KoderTeh-Boombox_Controller-1.1.6.zip"
+set "urls[5]=https://gcdn.thunderstore.io/live/repository/packages/KoderTeh-Boombox_Controller-1.1.9.zip"
 set "urls[6]=https://gcdn.thunderstore.io/live/repository/packages/anormaltwig-LateCompany-1.0.10.zip"
 set "urls[7]=https://gcdn.thunderstore.io/live/repository/packages/brigade-FreeBirdMod-1.0.0.zip"
-set "urls[8]=https://gcdn.thunderstore.io/live/repository/packages/no00ob-LCSoundTool-1.4.0.zip"
-set "urls[9]=https://gcdn.thunderstore.io/live/repository/packages/Clementinise-CustomSounds-2.2.0.zip"
-set "urls[10]=https://gcdn.thunderstore.io/live/repository/packages/SteamBlizzard-MetalPipeItems-1.0.0.zip"
-set "size=10"
+set "urls[8]=https://gcdn.thunderstore.io/live/repository/packages/no00ob-LCSoundTool-1.5.0.zip"
+set "urls[9]=https://gcdn.thunderstore.io/live/repository/packages/Clementinise-CustomSounds-2.3.1.zip"
+set "urls[10]=https://gcdn.thunderstore.io/live/repository/packages/PanHouse-LethalClunk-1.1.1.zip"
+set "urls[11]=https://gcdn.thunderstore.io/live/repository/packages/x753-More_Suits-1.4.1.zip"
+set "size=11"
 set "bepinEx=https://gcdn.thunderstore.io/live/repository/packages/BepInEx-BepInExPack-5.4.2100.zip"
 
 echo Tip: To copy a folder's location, shift+right click the folder and press 'Copy as path.'
@@ -74,8 +75,6 @@ echo Performing moves on mods that are not packaged correctly...
 move "!extractPath!\YippeeMod.dll" "!extractPath!\BepInEx\plugins"
 move "!extractPath!\yippeesound" "!extractPath!\BepInEx\plugins"
 move "!extractPath!\NicholaScott.BepInEx.RuntimeNetcodeRPCValidator.dll" "!extractPath!\BepInEx\plugins"
-move "!extractPath!\metal_pipe.mp3" "!extractPath!\BepInEx\plugins"
-move "!extractPath!\MetalPipeItems.dll" "!extractPath!\BepInEx\plugins"
 echo Complete.
 
 echo Cleaning up downloaded artifacts...
